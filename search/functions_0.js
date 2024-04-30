b64086d0d7ca64ddb0e7287b0bde9bdd34491dba
MeijisIrlnd <MeijisIrlnd@users.noreply.github.com>
2024-04-30 22:42:17 +0000

    deploy: 98fa9b90c2e50548a62015dcdb253dda7d04bff5

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['add_0',['add',['../namespacemarvin_1_1vecops.html#a0666a6ca3f7004aaa746f7d7950707b8',1,'marvin::vecops::add(T *lhs, const T *rhs, size_t size) noexcept'],['../namespacemarvin_1_1vecops.html#ae5317955cac6ed0b7984454d1d952934',1,'marvin::vecops::add(T *arr, T scalar, size_t size) noexcept'],['../namespacemarvin_1_1vecops.html#a0910dc42cdc8fa3989b93b442e7c6183',1,'marvin::vecops::add(T &amp;lhs, const T &amp;rhs) noexcept'],['../namespacemarvin_1_1vecops.html#a4f7ca29b0540ca4d24a842a65d6056f4',1,'marvin::vecops::add(T &amp;arr, typename T::value_type scalar) noexcept']]],
+  ['add_0',['add',['../namespacemarvin_1_1vecops.html#a0666a6ca3f7004aaa746f7d7950707b8',1,'marvin::vecops::add(T *lhs, const T *rhs, size_t size) noexcept'],['../namespacemarvin_1_1vecops.html#ae5317955cac6ed0b7984454d1d952934',1,'marvin::vecops::add(T *arr, T scalar, size_t size) noexcept'],['../namespacemarvin_1_1vecops.html#ad9be64e672966224d57e50cf4349131a',1,'marvin::vecops::add(T &amp;lhs, const T &amp;rhs) noexcept'],['../namespacemarvin_1_1vecops.html#aa6835452eb3e548becf9fedf6a977416',1,'marvin::vecops::add(T &amp;arr, typename T::value_type scalar) noexcept']]],
   ['allpass_1',['allpass',['../namespacemarvin_1_1dsp_1_1filters_1_1rbj.html#a018d81b21c5e991bb6d6778a2b604fe0',1,'marvin::dsp::filters::rbj']]]
 ];
