c87c02be196cd7b8bda7cd8edc22264223dceb61
MeijisIrlnd <MeijisIrlnd@users.noreply.github.com>
2024-05-02 18:25:43 +0000

    deploy: b2baf111f8c641cd75847ae02601bbacb46fdf8e

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['gaintodb_0',['gainToDb',['../namespacemarvin_1_1math.html#ad298b070cf86f74f306f824d265311e9',1,'marvin::math']]],
+  ['gaintodb_0',['gainToDb',['../namespacemarvin_1_1math.html#aa1bc6e54bd3d8abb587555b24648353b',1,'marvin::math']]],
   ['generate_1',['generate',['../classmarvin_1_1utils_1_1Random.html#ae64dd30118e742a805770b3cdba3b42f',1,'marvin::utils::Random']]],
   ['get_2',['get',['../classmarvin_1_1library_1_1PropagateConst.html#acb8104a4a6dbf10a979bd490e3fc99cc',1,'marvin::library::PropagateConst::get()'],['../classmarvin_1_1library_1_1PropagateConst.html#a4bd97a733ab2d96a96a7971204ab4630',1,'marvin::library::PropagateConst::get() const']]],
   ['getarrayofreadpointers_3',['getArrayOfReadPointers',['../structmarvin_1_1audiobasics_1_1BufferView.html#a917c342888f72a8aa623a573d0bd72af',1,'marvin::audiobasics::BufferView']]],
