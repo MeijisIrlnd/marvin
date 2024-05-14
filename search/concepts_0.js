260953dfc578ea286b1636ecf78c3f0a9cff44af
MeijisIrlnd <MeijisIrlnd@users.noreply.github.com>
2024-05-14 22:50:55 +0000

    deploy: abf319af8a85877246eba7e9a4a5ae1740679289

--- search/concepts_0.js
+++ search/concepts_0.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['marvin_3a_3aarraylike_0',['ArrayLike',['../conceptmarvin_1_1ArrayLike.html',1,'marvin']]],
-  ['marvin_3a_3afloattype_1',['FloatType',['../conceptmarvin_1_1FloatType.html',1,'marvin']]],
-  ['marvin_3a_3anumerictype_2',['NumericType',['../conceptmarvin_1_1NumericType.html',1,'marvin']]],
-  ['marvin_3a_3asmartpointertype_3',['SmartPointerType',['../conceptmarvin_1_1SmartPointerType.html',1,'marvin']]]
+  ['marvin_3a_3afloatarraylike_1',['FloatArrayLike',['../conceptmarvin_1_1FloatArrayLike.html',1,'marvin']]],
+  ['marvin_3a_3afloattype_2',['FloatType',['../conceptmarvin_1_1FloatType.html',1,'marvin']]],
+  ['marvin_3a_3anumerictype_3',['NumericType',['../conceptmarvin_1_1NumericType.html',1,'marvin']]],
+  ['marvin_3a_3asmartpointertype_4',['SmartPointerType',['../conceptmarvin_1_1SmartPointerType.html',1,'marvin']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['add_0',['add',['../namespacemarvin_1_1vecops.html#a0666a6ca3f7004aaa746f7d7950707b8',1,'marvin::vecops::add(T *lhs, const T *rhs, size_t size) noexcept'],['../namespacemarvin_1_1vecops.html#ae5317955cac6ed0b7984454d1d952934',1,'marvin::vecops::add(T *arr, T scalar, size_t size) noexcept'],['../namespacemarvin_1_1vecops.html#ad9be64e672966224d57e50cf4349131a',1,'marvin::vecops::add(T &amp;lhs, const T &amp;rhs) noexcept'],['../namespacemarvin_1_1vecops.html#aa6835452eb3e548becf9fedf6a977416',1,'marvin::vecops::add(T &amp;arr, typename T::value_type scalar) noexcept']]],
+  ['add_0',['add',['../namespacemarvin_1_1vecops.html#a0666a6ca3f7004aaa746f7d7950707b8',1,'marvin::vecops::add(T *lhs, const T *rhs, size_t size) noexcept'],['../namespacemarvin_1_1vecops.html#ae5317955cac6ed0b7984454d1d952934',1,'marvin::vecops::add(T *arr, T scalar, size_t size) noexcept'],['../namespacemarvin_1_1vecops.html#a3e1dd0df572c01fb04ce2a8e31f0eeec',1,'marvin::vecops::add(T &amp;lhs, T &amp;rhs) noexcept'],['../namespacemarvin_1_1vecops.html#a5da9a5b05086f32f729c1edafb044f17',1,'marvin::vecops::add(T &amp;arr, typename T::value_type scalar) noexcept']]],
   ['allpass_1',['allpass',['../namespacemarvin_1_1dsp_1_1filters_1_1rbj.html#a018d81b21c5e991bb6d6778a2b604fe0',1,'marvin::dsp::filters::rbj']]]
 ];
