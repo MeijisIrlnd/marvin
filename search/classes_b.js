910f1fd7db5122e576f24b9aacf0f3a6898f1f32
MeijisIrlnd <MeijisIrlnd@users.noreply.github.com>
2024-05-19 02:22:33 +0000

    deploy: 872d39e2a5f55cd770c914979d3e296d1c5ac8cb

--- search/classes_b.js
+++ search/classes_b.js
@@ -9,5 +9,5 @@ var searchData=
   ['squareoscillator_6',['SquareOscillator',['../classmarvin_1_1oscillators_1_1SquareOscillator.html',1,'marvin::oscillators']]],
   ['squareoscillator_3c_20sampletype_2c_20bandlimiting_3a_3aoff_20_3e_7',['SquareOscillator&lt; SampleType, Bandlimiting::Off &gt;',['../classmarvin_1_1oscillators_1_1SquareOscillator.html',1,'marvin::oscillators']]],
   ['strideview_8',['StrideView',['../classmarvin_1_1containers_1_1StrideView.html',1,'marvin::containers']]],
-  ['swapbuffer_9',['SwapBuffer',['../classmarvin_1_1audiobasics_1_1SwapBuffer.html',1,'marvin::audiobasics']]]
+  ['swapbuffer_9',['SwapBuffer',['../classmarvin_1_1containers_1_1SwapBuffer.html',1,'marvin::containers']]]
 ];
