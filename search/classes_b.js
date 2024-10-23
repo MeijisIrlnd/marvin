f97e4febe9b267696295358f985f8ed91a9293dc
MeijisIrlnd <MeijisIrlnd@users.noreply.github.com>
2024-10-23 19:05:22 +0000

    deploy: abef239a8065cb91d0e6c41d0367756df5172ae1

--- search/classes_b.js
+++ search/classes_b.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['propagateconst_0',['PropagateConst',['../classmarvin_1_1library_1_1PropagateConst.html',1,'marvin::library']]],
-  ['pulseoscillator_1',['PulseOscillator',['../classmarvin_1_1dsp_1_1oscillators_1_1PulseOscillator.html',1,'marvin::dsp::oscillators']]],
-  ['pulseoscillator_3c_20sampletype_2c_20bandlimiting_3a_3aoff_20_3e_2',['PulseOscillator&lt; SampleType, Bandlimiting::Off &gt;',['../classmarvin_1_1dsp_1_1oscillators_1_1PulseOscillator.html',1,'marvin::dsp::oscillators']]]
+  ['precomputedwindow_0',['PrecomputedWindow',['../classmarvin_1_1math_1_1windows_1_1PrecomputedWindow.html',1,'marvin::math::windows']]],
+  ['propagateconst_1',['PropagateConst',['../classmarvin_1_1library_1_1PropagateConst.html',1,'marvin::library']]],
+  ['pulseoscillator_2',['PulseOscillator',['../classmarvin_1_1dsp_1_1oscillators_1_1PulseOscillator.html',1,'marvin::dsp::oscillators']]],
+  ['pulseoscillator_3c_20sampletype_2c_20bandlimiting_3a_3aoff_20_3e_3',['PulseOscillator&lt; SampleType, Bandlimiting::Off &gt;',['../classmarvin_1_1dsp_1_1oscillators_1_1PulseOscillator.html',1,'marvin::dsp::oscillators']]]
 ];
